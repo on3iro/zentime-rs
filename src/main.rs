0796fd508d212b1c11f1b11d068add92d0c0d98e
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-10-22 21:14:31 +0200

    Add very basic timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
+use spin_sleep::sleep;
 use std::path::PathBuf;
+use std::time;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None, arg_required_else_help(true))]
@@ -35,6 +37,13 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
 
+    // TODO
+    // 1. Add crossterm
+    // 2. Split into rendering and input threads
+    // 3. Refactor
+    // 4. Add play/pause
+    // 5. Add configuration parsing
+
     match &cli.command {
         Some(Commands::Run { detached, config }) => {
             if *detached {
@@ -46,10 +55,26 @@ fn main() {
                 // TODO
                 println!("Read custom config file...");
             }
+
+            // 25minutes
+            let mut remaining_time = 60;
+
+            while remaining_time > 0 {
+                remaining_time -= 1;
+                let time = seconds_to_time(remaining_time);
+                println!("{}", time);
+                sleep(time::Duration::new(1, 0));
+            }
         }
 
         Some(_Commands) => {}
 
         None => {}
     }
+}
+
+fn seconds_to_time(duration: u16) -> String {
+    let min = duration / 60;
+    let sec = duration % 60;
+    format!("{:02}:{:02}", min, sec)
 }
