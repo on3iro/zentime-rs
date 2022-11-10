dba7183b8c69433f14044ddfda4d481e96eded3a
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-11-10 12:33:38 +0100

    REFACTOR: Extract lib

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+#![warn(missing_docs)]
+
+//! Zentime cli
+
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+use subcommands::run::run;
+
+mod config;
+mod events;
+mod input;
+mod notification;
+mod sound;
+mod subcommands;
+mod timer;
+mod util;
+mod view;
+
+/// Starts the timer or attaches to an already running timer
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long, default_value = "~/.config/zentime/zentime.toml")]
+    config: String,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+/// Available cli sub commands
+#[derive(Subcommand)]
+pub enum Commands {}
+
+/// Runs the specified zentime cli command
+pub fn run_cli() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        // TODO
+        Some(_commands) => {}
+
+        None => run(&cli.config),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,5 @@
-use clap::{Parser, Subcommand};
-use std::path::PathBuf;
-use subcommands::run::run;
-
-mod config;
-mod events;
-mod input;
-mod notification;
-mod sound;
-mod subcommands;
-mod timer;
-mod util;
-mod view;
-
-/// Starts the timer or attaches to an already running timer
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[arg(short, long, action = clap::ArgAction::SetTrue)]
-    detached: bool,
-
-    /// Sets a custom config file
-    #[arg(short, long, default_value = "~/.config/zentime/zentime.toml")]
-    config: String,
-
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-pub enum Commands {
-    /// Restarts an already running timer and applies the current configuration.
-    /// If no timer exists it simply starts a new timer.
-    Restart {
-        /// Sets a custom config file
-        #[arg(short, long, value_name = "~/.config/zentime/zentime.toml")]
-        config: Option<PathBuf>,
-    },
-
-    /// Opens the configuration file inside the default terminal editor
-    Configure {},
-}
+use zentime_rs::run_cli;
 
 fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        // TODO
-        Some(_Commands) => {}
-
-        None => run(&cli.config),
-    }
+    run_cli();
 }
