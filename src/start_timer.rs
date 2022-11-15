90e036994ebab8d65e9789e5cf80e62062c06afa
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-11-15 22:20:00 +0100

    REFACTOR: Change app code to reflect timer refactoring

--- src/start_timer.rs
+++ src/start_timer.rs
@@ -2,43 +2,12 @@ use crate::config::{create_config, Config};
 use crate::input::TerminalInputThread;
 use crate::notification::dispatch_notification;
 use crate::view::TerminalRenderer;
+use crate::TerminalEvent;
+use std::time::Duration;
+use zentime_rs_timer::events::AppAction;
 use zentime_rs_timer::Timer;
 
-use std::sync::mpsc;
-
-// self.view_sender
-// .send(TerminalEvent::View(ViewState {
-// is_break: self.shared_state.is_break,
-// round: self.shared_state.round,
-// time: seconds_to_time(time),
-// }))
-// .context("View sender could not send")?;
-
-// let action = match self
-// .app_action_receiver
-// .recv_timeout(Duration::from_secs(1))
-// {
-// Ok(action) => action,
-// Err(RecvTimeoutError::Disconnected) => AppAction::Quit,
-// _ => AppAction::None,
-// };
-
-// self.view_sender
-// .send(TerminalEvent::View(ViewState {
-// is_break: self.shared_state.is_break,
-// round: self.shared_state.round,
-// time: seconds_to_time(time),
-// }))
-// .context("View sender could not send")?;
-
-// let action = match self
-// .app_action_receiver
-// .recv_timeout(Duration::from_secs(1))
-// {
-// Ok(action) => action,
-// Err(RecvTimeoutError::Disconnected) => AppAction::Quit,
-// _ => AppAction::None,
-// };
+use std::sync::mpsc::{self, RecvTimeoutError};
 
 pub fn start_timer(config_path: &str) {
     let config: Config = create_config(config_path)
@@ -52,12 +21,32 @@ pub fn start_timer(config_path: &str) {
     let render_thread_handle = TerminalRenderer::spawn(view_receiver, config.view);
 
     Timer::new(
-        terminal_input_receiver,
-        view_sender,
         config.timers,
         Box::new(move |_, msg| {
             // We simply discard errors here for now...
             dispatch_notification(config.notifications, msg).ok();
+        }),
+        Box::new(move |view_state| {
+            // Update the view
+            view_sender
+                .send(TerminalEvent::View(view_state))
+                .expect("Could not send to view");
+
+            // Quit handler for re-use in various match arms
+            let handle_quit = || {
+                view_sender
+                    .send(TerminalEvent::Quit)
+                    .expect("Could not send quit event");
+                Some(AppAction::Quit)
+            };
+
+            // Handle app actions and hand them to the timer caller
+            match terminal_input_receiver.recv_timeout(Duration::from_secs(1)) {
+                Ok(AppAction::Quit) => handle_quit(),
+                Ok(action) => Some(action),
+                Err(RecvTimeoutError::Disconnected) => handle_quit(),
+                _ => None,
+            }
         }),
     )
     .init()
