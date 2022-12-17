274f94a43d9b822ab9d3fac9700bd44d9059eeb1
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-12-17 21:36:58 +0100

    REFACTOR: Replace PartialConfigBuilder with usage of serde attributes and clap structs

--- src/default_cmd.rs
+++ src/default_cmd.rs
@@ -6,7 +6,6 @@ use sysinfo::Pid;
 use zentime_rs::client::start;
 use zentime_rs::config::create_base_config;
 use zentime_rs::config::Config;
-use zentime_rs::config::PartialConfigBuilder;
 
 use sysinfo::ProcessExt;
 use sysinfo::System;
@@ -64,7 +63,6 @@ pub async fn default_cmd(common_args: &CommonArgs, client_config: &ClientConfig)
     start(config).await;
 }
 
-// TODO try to get rid of all that boilerplate
 fn get_server_args(common_args: &CommonArgs) -> Vec<String> {
     let mut args: Vec<String> = vec![
         // Config path
@@ -72,37 +70,37 @@ fn get_server_args(common_args: &CommonArgs) -> Vec<String> {
         common_args.config.to_string(),
     ];
 
-    if let Some(enable_bell) = &common_args.server_config.enable_bell {
+    if let Some(enable_bell) = &common_args.server_config.notifications.enable_bell {
         args.push("--enable-bell".to_string());
         args.push(enable_bell.to_string());
     }
 
-    if let Some(volume) = &common_args.server_config.volume {
+    if let Some(volume) = &common_args.server_config.notifications.volume {
         args.push("--volume".to_string());
         args.push(volume.to_string());
     }
 
-    if let Some(show_notification) = &common_args.server_config.show_notification {
+    if let Some(show_notification) = &common_args.server_config.notifications.show_notification {
         args.push("--show-notification".to_string());
         args.push(show_notification.to_string());
     }
 
-    if let Some(timer) = &common_args.server_config.timer {
+    if let Some(timer) = &common_args.server_config.timers.timer {
         args.push("--timer".to_string());
         args.push(timer.to_string());
     }
 
-    if let Some(minor_break) = &common_args.server_config.minor_break {
+    if let Some(minor_break) = &common_args.server_config.timers.minor_break {
         args.push("--minor-break".to_string());
         args.push(minor_break.to_string());
     }
 
-    if let Some(major_break) = &common_args.server_config.major_break {
+    if let Some(major_break) = &common_args.server_config.timers.major_break {
         args.push("--major-break".to_string());
         args.push(major_break.to_string());
     }
 
-    if let Some(intervals) = &common_args.server_config.intervals {
+    if let Some(intervals) = &common_args.server_config.timers.intervals {
         args.push("--intervals".to_string());
         args.push(intervals.to_string())
     }
@@ -111,14 +109,6 @@ fn get_server_args(common_args: &CommonArgs) -> Vec<String> {
 }
 
 fn get_client_config(config_path: &str, client_config: &ClientConfig) -> Config {
-    let mut client_config_builder = PartialConfigBuilder::new();
-
-    if let Some(interface) = &client_config.interface {
-        client_config_builder.interface(interface.to_string());
-    }
-
-    let client_config = client_config_builder.build();
-
     create_base_config(config_path)
         .merge(Serialized::defaults(client_config))
         .extract()
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use env_logger::Env;
 
 mod default_cmd;
 mod subcommands;
+use serde::{Deserialize, Serialize};
 use subcommands::server::{start_daemonized, status, stop};
 
 #[derive(clap::Args)]
@@ -16,41 +17,81 @@ pub struct CommonArgs {
     server_config: ServerConfig,
 }
 
-#[derive(clap::Args, Clone, Debug)]
-pub struct ServerConfig {
+/// This should match [Config::NotificationConfig], but makes fields optional, so that they are not
+/// required by clap. If no value is provided and therefore the `Option` is `None`, we skip
+/// serializing the value.
+#[derive(clap::Args, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(rename(serialize = "NotificationConfig"))]
+struct ClapNotificationConfig {
     /// Enable/Disable bell
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub enable_bell: Option<bool>,
 
     /// Notification bell volume
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub volume: Option<f32>,
 
     /// Show OS-notification
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub show_notification: Option<bool>,
+}
 
+/// This should match [zentime-rs-timer::config::TimerConfig], but makes fields optional, so that they are not
+/// required by clap. If no value is provided and therefore the `Option` is `None`, we skip
+/// serializing the value.
+#[derive(clap::Args, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(rename(serialize = "TimerConfig"))]
+struct ClapTimerConfig {
     /// Timer in seconds
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub timer: Option<u64>,
 
     /// Minor break time in seconds
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub minor_break: Option<u64>,
 
     /// Major break time in seconds
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub major_break: Option<u64>,
 
     /// Intervals before major break
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long)]
     pub intervals: Option<u64>,
 }
 
-#[derive(clap::Args, Clone, Debug)]
-pub struct ClientConfig {
+#[derive(clap::Args, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(rename(serialize = "Config"))]
+pub struct ServerConfig {
+    #[command(flatten)]
+    timers: ClapTimerConfig,
+
+    #[command(flatten)]
+    notifications: ClapNotificationConfig,
+}
+
+/// This should match [Config::ViewConfig], but makes fields optional, so that they are not
+/// required by clap. If no value is provided and therefore the `Option` is `None`, we skip
+/// serializing the value.
+#[derive(clap::Args, Serialize, Deserialize, Clone, Debug)]
+#[serde(rename(serialize = "ViewConfig"))]
+struct ClapViewConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[arg(long, short = 'i', long_help = include_str!("./ViewConfig.md"), verbatim_doc_comment)]
     pub interface: Option<String>,
+}
+
+#[derive(clap::Args, Serialize, Deserialize, Clone, Debug)]
+#[serde(rename(serialize = "Config"))]
+pub struct ClientConfig {
+    #[command(flatten)]
+    view: ClapViewConfig,
 }
 
 /// Starts the timer or attaches to an already running timer
