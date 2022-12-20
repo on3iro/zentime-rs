708ec3839cfe9d17c020be53592874e8427fec74
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-12-20 22:16:58 +0100

    REFACTOR: one shot connection

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,7 @@
 //! Code related to zentime terminal clients (e.g. async connection handling, terminal io etc.)
 
 mod connection;
+pub mod one_shot_connection;
 pub mod start;
 pub mod terminal_io;
 
---
+++ src/client/one_shot_connection.rs
@@ -0,0 +1,28 @@
+use crate::ipc::get_socket_name;
+use crate::server::status::server_status;
+use crate::server::status::ServerStatus;
+use interprocess::local_socket::tokio::LocalSocketStream;
+use interprocess::local_socket::tokio::OwnedReadHalf;
+use interprocess::local_socket::tokio::OwnedWriteHalf;
+
+/// Creates a connection to the zentime server (if one is running) and returns
+/// a tuple of [OwnedReadHalf] and an [OwnedWriteHalf].
+/// If no server is running the current process is terminated.
+///
+/// NOTE:
+/// If you just want to read from the server, you still need to write [ClientToServerMsg::Sync]
+/// first and make sure that the writer isn't being dropped before you read. Otherwise you will
+/// encount EOF on the socket!
+pub async fn one_shot_connection() -> anyhow::Result<(OwnedReadHalf, OwnedWriteHalf)> {
+    // check if server is running -> if not, quit
+    if server_status() == ServerStatus::Stopped {
+        println!("No zentime server running");
+        std::process::exit(0);
+    }
+
+    // connect to server
+    let socket_name = get_socket_name();
+    let connection = LocalSocketStream::connect(socket_name).await?;
+
+    Ok(connection.into_split())
+}
